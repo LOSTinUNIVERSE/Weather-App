0f908481521fdbfc46cd50db6b8842bf5d43c765
abdulla <abdullaabdullaev0803@gmail.com>
2023-04-12 14:03:43 +0300

    add function to display info in console

--- src/domElements.js
+++ src/domElements.js
@@ -2,6 +2,14 @@ console.log('hi');
 const domElements = (() => {
     const inputLocation = document.getElementById('inputLocation')
     const submitBtn = document.getElementById('submitBtn')
-    return { inputLocation, submitBtn }
+    const location = document.getElementById('location')
+    const text = document.getElementById('text')
+    const icon = document.getElementById('icon')
+    const feelsLike = document.getElementById('feelsLike')
+    const wind = document.getElementById('wind')
+    return {
+        inputLocation, submitBtn,
+        location, text, icon, feelsLike, wind
+    }
 })()
 export default domElements
--- src/index.js
+++ src/index.js
@@ -3,28 +3,42 @@ import domElements from './domElements'
 const dataFromApi = (() => {
     let convertedData = ''
     const neededInfo = {}
+    function populatePage() {
+        domElements.location.textContent = `${neededInfo.location.country}, ${neededInfo.location.name}`
+        domElements.text.textContent = `${neededInfo.condition.text}`
+        domElements.feelsLike.textContent = `feels like: ${neededInfo.feelsLike}`
+        domElements.wind.textContent = `wind speed: ${neededInfo.wind}`
+    }
+
     const getNeededInfo = (data) => {
+        console.log(data);
         const currentOfData = data.current
         const locationOfData = data.location
+        neededInfo.condition = {}
         neededInfo.cloud = currentOfData.cloud
+        neededInfo.condition.text = currentOfData.condition.text
+        neededInfo.condition.icon = currentOfData.condition.icon
         neededInfo.feelsLike = currentOfData.feelslike_c
         neededInfo.wind = currentOfData.wind_kph
         neededInfo.location = {}
         neededInfo.location.country = locationOfData.country
         neededInfo.location.name = locationOfData.name
         console.log(neededInfo);
+        populatePage()
+        // return neededInfo
     }
 
     async function getData(location) {
         const data = await fetch(`https:api.weatherapi.com/v1/current.json?key=fde9d000471240ee9d472224231104&q=${location}`, { mode: 'cors' })
         convertedData = await data.json()
         getNeededInfo(await convertedData)
+        return convertedData
     }
     return { getData, getNeededInfo, neededInfo }
 
 })()
 function triggerGetData(event) {
-    dataFromApi.getData(domElements.inputLocation.value);
+    dataFromApi.getData(domElements.inputLocation.value)
     event.preventDefault()
 }
 domElements.submitBtn.addEventListener('click', triggerGetData, false)
