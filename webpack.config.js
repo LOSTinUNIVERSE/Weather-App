b07c4d2c3b8fe0465090a8f25665d010794203fb
abdulla <abdullaabdullaev0803@gmail.com>
2023-04-12 21:01:58 +0300

    make changes to files

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
@@ -20,15 +21,7 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
 
-            },
         ],
     },
 };
